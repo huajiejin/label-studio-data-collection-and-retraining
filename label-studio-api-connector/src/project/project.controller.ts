7e1872cb883cb9f6b8c8dd436eccb0da6e58ab5a
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2023-12-01 12:57:28 -0500

    a commit should be pushed months ago

--- label-studio-api-connector/src/project/project.controller.ts
+++ label-studio-api-connector/src/project/project.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Param } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { lastValueFrom } from 'rxjs';
@@ -10,9 +10,15 @@ export class ProjectController {
 	constructor(private readonly projectService: ProjectService) {}
 
 	@Post()
-	@ApiOperation({ summary: 'Create project on Lable Studio' })
+	@ApiOperation({ summary: 'Create project on Label Studio' })
 	@ApiOkResponse({ description: 'Returns the id of the project.' })
 	async createProject(@Body() dto: CreateProjectDto) {
 		return await lastValueFrom(this.projectService.createProject(dto));
+	}
+
+	@Post('sample')
+	@ApiOperation({ summary: 'Create sample project on Label Studio'})
+	createSampleProject(@Param('title') title: string) {
+		return this.projectService.createSampleProject({ title });
 	}
 }
--- label-studio-api-connector/src/project/project.service.ts
+++ label-studio-api-connector/src/project/project.service.ts
@@ -1,14 +1,17 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AxiosRequestConfig } from 'axios';
-import { map } from 'rxjs';
+import { from, map, switchMap, tap } from 'rxjs';
 import { CreateProjectDto } from './create-project.dto';
+import { promises as fs } from 'fs';
 
 @Injectable()
-export class ProjectService {
+export class ProjectService implements OnApplicationBootstrap {
 	private readonly logger = new Logger(ProjectService.name);
-	private labelStudioRequestConfig: AxiosRequestConfig
+	private labelStudioRequestConfig: AxiosRequestConfig;
+	private labelStudioTasksReadingPath: string;
+	private labelStudioTasksSavingPath: string;
 
 	constructor(
 		private readonly httpService: HttpService,
@@ -22,16 +25,82 @@ export class ProjectService {
 			headers: { Authorization: `Token ${LABEL_STUDIO_USER_TOKEN}` },
 		};
 		this.logger.debug(`labelStudioRequestConfig is ${JSON.stringify(this.labelStudioRequestConfig)}`);
+
+		const LS_BASE_DATA_DIR = this.configService.get<string>('LS_BASE_DATA_DIR', '/label-studio/data');
+		this.labelStudioTasksReadingPath = `${LS_BASE_DATA_DIR}/label-studio-tasks`;
+
+		const LSDCAR_BASE_DATA_DIR = this.configService.get<string>('LSDCAR_BASE_DATA_DIR');
+		this.labelStudioTasksSavingPath = `${LSDCAR_BASE_DATA_DIR}/label-studio-tasks`;
 	}
 	
 	createProject(dto: CreateProjectDto) {
 		return this.httpService
 			.post<LabelStudioCreateProjectResponse>('/api/projects', dto, this.labelStudioRequestConfig)
-			.pipe(map(res => res.data))
+			.pipe(
+				map(res => res.data),
+				tap({
+					next: value => this.logger.debug(`createProject ${JSON.stringify(value)}`),
+					error: err => this.logger.error(`Error createProject`, err),
+				}),
+			);
+	}
+
+	createLocalfilesStorage(projectID: number) {
+		const readingPath = `${this.labelStudioTasksReadingPath}/${projectID}`;
+		const savingPath = `${this.labelStudioTasksSavingPath}/${projectID}`;
+		const payload = {
+			project: projectID,
+			title: `Localfiles Storage for Tasks of Project ${projectID}`,
+			path: readingPath,
+			regex_filter: '.*json',
+		};
+		return from(this.createFolder(savingPath))
+			.pipe(
+				tap({
+					next: value => this.logger.debug(`createFolder ${value}`),
+					error: err => this.logger.error(`Error createFolder`, err),
+				}),
+				switchMap(() => this.httpService.post<LabelStudioCreateLocalfilesStorageResponse>('/api/storages/localfiles', payload, this.labelStudioRequestConfig)),
+				map(resp => resp.data),
+				tap({
+					next: value => this.logger.debug(`post /api/storages/localfiles ${JSON.stringify(value)}`),
+					error: err => this.logger.error(`Error post /api/storages/localfiles ${JSON.stringify(payload)}`, err),
+				}),
+			);
+	}
+
+	createSampleProject(dto: CreateProjectDto) {
+		return this.createProject(dto)
+			.pipe(
+				switchMap(resp => this.createLocalfilesStorage(resp.id)),
+			);
+	}
+
+	async onApplicationBootstrap() {
+		// await this.createFolder(this.labelStudioTasksPath);
+	}
+
+	async createFolder(path: string) {
+		await fs.mkdir(path, { recursive: true })
+			.then(() => {
+				this.logger.log(`Folder created ${path}`);
+			})
+			.catch((err) => {
+				this.logger.error(`Error creating folder ${path}`, err);
+			});
 	}
 }
 
 export interface LabelStudioCreateProjectResponse {
 	id: number
 	title: string
+}
+
+export interface LabelStudioCreateLocalfilesStorageResponse {
+	id: number
+	project: number
+	title: string
+	type: string
+	path: string
+	regex_filter: string
 }
